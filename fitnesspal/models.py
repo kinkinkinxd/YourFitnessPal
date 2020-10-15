63131055a99cc5781b3e957f1c92c86195d57112
jeanyjean <purich.double@gmail.com>
2020-10-15 18:13:27 +0700

    Add user model

--- fitnesspal/models.py
+++ fitnesspal/models.py
@@ -1,3 +1,26 @@
-from django.db import models
+"""Models class for database."""
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+class Calories(models.Model):
+    food_name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.food_name
+
+class Exercise(models.Model):
+    exercise_name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.exercise_name
+
+class User(models.Model):
+   user_name = models.CharField(max_length=40)
+   password = models.CharField(max_length=20)
+   food_eat = models.ManyToManyField(Calories)
+   exercise = models.ManyToManyField(Exercise)
+
+   def __str__(self):
+        return self.user_name
