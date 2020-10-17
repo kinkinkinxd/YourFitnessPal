b4d926320beb02b1a9e8816e1a17d53e4d802e24
kinkinkinxd <kinkinkinxd@gmail.com>
2020-10-17 19:38:33 +0700

    update draft for home page

--- fitnessSite/urls.py
+++ fitnessSite/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include,path
+from django.urls import include, path
+
 
 urlpatterns = [
-    path('', include('fitnesspal.urls')),
+    # path('', views.index),
     path('admin/', admin.site.urls),
+    path('', include('fitnesspal.urls')),
 ]
