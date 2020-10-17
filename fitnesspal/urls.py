8aacb221dc1fd9552a0ad62bddf14c59ddb08855
peerasu <peerasu.w@ku.th>
2020-10-17 23:26:31 +0700

    finished login page

--- fitnesspal/urls.py
+++ fitnesspal/urls.py
@@ -1,10 +1,12 @@
 """urls routing for fitnesspal."""
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 
 from . import views
 
 
 app_name = 'fitnesspal'
-urlpatterns = [
+urlpatterns = [path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     
 ]
