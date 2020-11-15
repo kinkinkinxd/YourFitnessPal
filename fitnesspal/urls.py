ccb8fac682529f7abc3509ef3e4494d87262d9d5
Kittitouch Ingkasompob <55922403+kinkinkinxd@users.noreply.github.com>
2020-11-16 01:14:07 +0700

    Update urls.py
    fix error.

--- fitnesspal/urls.py
+++ fitnesspal/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('calories/', views.calories, name='calories'),
     path('calories/calculate/', views.calculate_calories, name='calculate_calories'),
     path('exercise/', views.exercise, name='exercise'),
-    path('exercise/calculate/', views.exercise_calories_burn, name='calculate_exercise')
+    path('exercise/calculate/', views.exercise_calories_burn, name='calculate_exercise'),
     path('profile/', views.profile, name="profile"),
     path('calories/add/', views.add_food_calories, name="add_food"),
 ]
