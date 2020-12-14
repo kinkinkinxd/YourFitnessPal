28bc1be39a2a57fb460fb2e4c84906cdaa372fd3
PanitanPlengkham <panitan.pl@ku.th>
2020-12-15 02:12:39 +0700

    Decorate Site

--- fitnessSite/settings.py
+++ fitnessSite/settings.py
@@ -26,9 +26,9 @@ env.read_env()
 SECRET_KEY = env('SECRET_KEY', default='secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['your-fitnesspal.herokuapp.com']
+ALLOWED_HOSTS = ['your-fitnesspal.herokuapp.com','127.0.0.1']
 
 # Application definition
 
