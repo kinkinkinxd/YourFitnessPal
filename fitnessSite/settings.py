0707e66dcade8ef533103dcfda8cc50ca21d3ba2
jeanyjean <purich.double@gmail.com>
2020-12-16 20:53:46 +0700

    Final deploy

--- fitnessSite/settings.py
+++ fitnessSite/settings.py
@@ -27,7 +27,7 @@ env.read_env()
 SECRET_KEY = env('SECRET_KEY', default='secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = ['your-fitnesspal.herokuapp.com', '127.0.0.1', 'localhost']
